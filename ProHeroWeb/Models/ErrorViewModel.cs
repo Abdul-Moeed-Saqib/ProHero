caaaa637351df2ffc61a8664e5d82c6e74e92cf1
Abdul Moeed Saqib <sahilsaqib3333@gmail.com>
2022-07-05 17:49:12 -0400

    Add project files.

---
+++ ProHeroWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProHeroWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
